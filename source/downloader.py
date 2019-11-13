4079f128769330a0f1ff7b8709a9cb24a8b2894b
Antonio <40355800+manjavacas@users.noreply.github.com>
2019-11-13 09:33:24 +0100

    Add files via upload

---
+++ source/downloader.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+'''
+    Youtube video downloader
+'''
+
+# Libs
+import sys
+import Ice
+
+Ice.loadSlice('trawlnet.ice')
+import TrawlNet
+
+__author__ = 'Antonio Manjavacas'
+__license__ = 'GPL'
+
+
+class DownloaderServer(Ice.Application):
+    '''
+    Downloader task receiver
+    '''
+
+    def run(self, argv):
+
+        broker = self.communicator()
+        servant = DownloaderI()
+
+        adapter = broker.createObjectAdapter("DownloaderAdapter")
+        proxy = adapter.add(servant, broker.stringToIdentity("downloader1"))
+
+        print('[DOWNLOADER] showing proxy...\n{0}'.format(proxy))
+
+        adapter.activate()
+        self.shutdownOnInterrupt()
+        broker.waitForShutdown()
+
+        return 0
+
+
+class DownloaderI(TrawlNet.Downloader):
+    '''
+    Downloader servant
+    '''
+    
+    n = 0
+
+    def addDownloadTask(self, url, current=None):
+
+        print('[DOWNLOADER] adds download task {0}: {1}'.format(self.n, url))
+        sys.stdout.flush()
+        self.n += 1
+
+        return 0
+
+
+SERVER = DownloaderServer()
+sys.exit(SERVER.main(sys.argv))
