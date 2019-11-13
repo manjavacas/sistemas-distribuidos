15e9e0cd90ec8b1dcb9c83ac1b12acb322f0ed3a
Antonio <40355800+manjavacas@users.noreply.github.com>
2019-11-13 09:38:52 +0100

    Add files via upload

---
+++ source/client.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+'''
+    Client for sending requests to download videos
+'''
+
+# Libs
+import sys
+import Ice
+
+Ice.loadSlice('trawlnet.ice')
+import TrawlNet
+
+__author__ = 'Antonio Manjavacas'
+__license__ = 'GPL'
+
+
+class Client(Ice.Application):
+    '''
+    Client class which calls the orchestrator
+    '''
+
+    def run(self, argv):
+        proxy = self.communicator().stringToProxy(argv[1])
+        orchestrator = TrawlNet.OrchestratorPrx.checkedCast(proxy)
+
+        if not orchestrator:
+            raise RuntimeError('[CLIENT] error: invalid proxy')
+
+        print('[CLIENT] sending URL to {0}...'.format(argv[2]))
+        orchestrator.downloadTask(argv[2])
+
+        return 0
+
+
+if len(sys.argv) != 3:
+    print('[CLIENT] usage: client.py <proxy> <file-url>')
+    exit()
+
+CLIENT = Client()
+sys.exit(CLIENT.main(sys.argv))
