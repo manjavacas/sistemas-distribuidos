6593b7a8b823885c21bb80b9bc318d9fc2909eb7
manjavacas <Antonio.Manjavacas@alu.uclm.es>
2019-11-21 18:36:09 +0100

    Updated scripts

--- src/run_client.sh
+++ src/run_client.sh
@@ -2,7 +2,4 @@
 
 url="https://www.youtube.com/watch?v=W1b1laUX_hc"
 
-./client.py "$(head -1 orchestrator.out)" $url --Ice.Config=Client.config &
-PID=$!
-
-wait $PID
+./client.py "$(head -1 orchestrator.out)" $url --Ice.Config=Client.config
--- src/run_server.sh
+++ src/run_server.sh
@@ -3,9 +3,9 @@
 rm -f -r ./*.out /tmp/db/registry
 mkdir -p /tmp/db/registry
 
-icegridregistry --Ice.Config=Node.config & 
+icegridregistry --Ice.Config=Node.config &
 
-./downloader.py --Ice.Config=Downloader.config > downloader.out & 
+./downloader.py --Ice.Config=Downloader.config > downloader.out &
 
 sleep 1
 ./orchestrator.py "$(head -1 downloader.out)" --Ice.Config=Orchestrator.config > orchestrator.out
