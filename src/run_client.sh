7ad1162cf7b87062d079245f56296cded923bde4
manjavacas <Antonio.Manjavacas@alu.uclm.es>
2020-01-14 12:31:29 +0100

    Edited YoutubeDownloaderApp config

--- src/run_client.sh
+++ src/run_client.sh
@@ -2,15 +2,15 @@
 #
 
 echo "Downloading audio..."
-./client.py "orchestrator1 -t -e 1.1 @ ReplicatedOrchestratorAdapter" --download "https://www.youtube.com/watch?v=SSbBvKaM6sk" \
+./client.py orchestrator --download "https://www.youtube.com/watch?v=SSbBvKaM6sk" \
 --Ice.Config=client.config
 
 echo ""
 echo "List request..."
-./client.py "orchestrator1 -t -e 1.1 @ ReplicatedOrchestratorAdapter" \
+./client.py orchestrator \
 --Ice.Config=client.config
 
 echo ""
 echo "Init transfer..."
-./client.py "orchestrator1 -t -e 1.1 @ ReplicatedOrchestratorAdapter" --transfer "Blur - Song 2.mp3" \
+./client.py orchestrator --transfer "Blur - Song 2.mp3" \
 --Ice.Config=client.config
